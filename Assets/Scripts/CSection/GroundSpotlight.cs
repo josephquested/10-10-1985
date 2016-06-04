28a95ed1f3c968ef66fffa33985904bb3185aec3
Joseph Quested <josephquested@Josephs-MacBook-Pro.local>
2016-06-05 10:03:07 +1200

    add climbable model

---
+++ Assets/Scripts/CSection/GroundSpotlight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundSpotlight : MonoBehaviour {
+
+	[SerializeField] int timeDelay;
+	[SerializeField] Light spotlight;
+	[SerializeField] Transform playerTransform;
+
+	public void Activate () {
+		StartCoroutine(ActivateSpotlightDelay(timeDelay));
+	}
+
+	IEnumerator ActivateSpotlightDelay (int time) {
+		yield return new WaitForSeconds(time);
+		ToggleLights(true);
+	}
+
+	public void ToggleLights (bool state) {
+		spotlight.enabled = state;
+	}
+}
---
+++ Assets/Scripts/CSection/GroundTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundTrigger : MonoBehaviour {
+
+	[SerializeField] GroundSpotlight spotlight;
+
+	void OnTriggerEnter (Collider collider) {
+		if (collider.CompareTag("Player")) {
+			spotlight.Activate();
+		}
+	}
+}
