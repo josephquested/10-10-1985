c04ba746961af265621d70b4919bbd30622136a7
josephquested <josephquested@gmail.com>
2017-02-16 16:15:49 +1300

    add father model

---
+++ Assets/Scripts/CSection/SpermTail.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpermTail : MonoBehaviour {
+	Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		Vector3 movement = new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f));
+		rb.AddForce(movement);
+	}
+}
