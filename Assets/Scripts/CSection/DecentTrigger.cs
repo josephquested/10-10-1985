3904d1a5b1fe2ce4096be50a89fd462e2d8db928
josephquested <josephquested@gmail.com>
2017-02-17 10:25:11 +1300

    tweaked mother size and rotation

---
+++ Assets/Scripts/CSection/DecentTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecentTrigger : MonoBehaviour {
+	public Animator fatherAnimator;
+
+	void OnTriggerEnter (Collider collider)
+	{
+		if (collider.tag == "Player")
+		{
+			print("triggered!");
+			fatherAnimator.speed = 1;
+		}
+	}
+}
