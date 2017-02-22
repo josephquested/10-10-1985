dd2e7f9efa784385d7b20dc599b670c3dddb6a01
josephquested <josephquested@gmail.com>
2017-02-22 14:44:09 +1300

    starting on credit sequence

--- Assets/Scripts/CSection/EndTrigger.cs
+++ Assets/Scripts/CSection/EndTrigger.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EndTrigger : MonoBehaviour {
+	public AudioSource natureAudio;
+	public AudioSource playerAudio;
 
 	void OnTriggerEnter (Collider collider)
 	{
 		if (collider.tag == "Player")
 		{
 			GetComponent<FadeOut>().shouldFade = true;
+			StartCoroutine(FadeOutAudio());
 		}
+	}
+
+	IEnumerator FadeOutAudio ()
+	{
+		natureAudio.volume -= 0.01f;
+		playerAudio.volume -= 0.01f;
+		yield return new WaitForSeconds(0.01f);
 	}
 }
